8fb939c666204aec56204d07e31f2d73542e82fd
nepstech <majhintech@gmail.com>
2024-04-19 19:19:19 +0300

    nodejs project started

---
+++ app.js
@@ -0,0 +1,26 @@
+// const app = require("express")()
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const data = { name: "Manish", age: 22 };
+  res.render("home", { data });
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+app.get("/blog/create", (req, res) => {
+  res.render("createblog");
+});
+
+app.get("/blog/edit", (req, res) => {
+  res.send("Soon, here will be the edit form");
+});
+
+app.listen(3000, () => {
+  console.log("Server has started at port 3000");
+});
